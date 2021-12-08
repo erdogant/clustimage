173f3597222867d7b8cc85593edc7a6be6ed310a
erdogant <erdogant@gmail.com>
2021-12-08 22:11:54 +0100

    udpate version

--- clustimage/__init__.py
+++ clustimage/__init__.py
@@ -10,7 +10,7 @@ from clustimage.clustimage import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.3.5'
+__version__ = '1.3.6'
 
 # module level doc-string
 __doc__ = """
