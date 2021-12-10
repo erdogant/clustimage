67820ed68cb33601d16da1bdc06028393c6994a0
erdogant <erdogant@gmail.com>
2021-12-10 09:40:49 +0100

    update unittests

--- tests/test_clustimage.py
+++ tests/test_clustimage.py
@@ -63,31 +63,6 @@ class TestCLUSTIMAGE(unittest.TestCase):
         labels = cl.cluster()
         assert len(cl.results['labels'])==X['img'].shape[0]
 
-    def test_fit_transform(self):
-        # Example data
-        cl = Clustimage()
-        Xflowers = cl.import_example(data='flowers')
-        Xdigits = cl.import_example(data='mnist')
-        Xfaces = cl.import_example(data='faces')
-        # Parameters combinations to check
-        param_grid = {
-        	'method':['ahash', 'pca', 'hog', None],
-        	'embedding':['tsne', None],
-        	'cluster_space' : ['high', 'low'],
-        	'grayscale' : [True, False],
-            'dim' : [(8,8), (128,128), (256,256)],
-            'data' : [Xflowers, Xdigits]
-        	}
-        # Make the combinatinos
-        allNames = param_grid.keys()
-        combinations = list(it.product(*(param_grid[Name] for Name in allNames)))
-        # Iterate over all combinations
-        for i, combination in enumerate(combinations):
-            # init
-            cl = Clustimage(method=combination[0], embedding=combination[1], grayscale=combination[3], dim=combination[4])
-            # Preprocessing and feature extraction
-            assert cl.fit_transform(combination[5], cluster_space=combination[2])
-
     def test_cluster(self):
         cl = Clustimage()
         X = cl.import_example(data='flowers')
@@ -141,3 +116,29 @@ class TestCLUSTIMAGE(unittest.TestCase):
         # Cluster
         results = cl.fit_transform(X)
         assert np.all(np.isin([*cl.results.keys()], ['img', 'feat', 'xycoord', 'pathnames', 'filenames', 'labels']))
+
+    def test_fit_transform(self):
+        # Example data
+        cl = Clustimage()
+        Xflowers = cl.import_example(data='flowers')
+        Xdigits = cl.import_example(data='mnist')
+        Xfaces = cl.import_example(data='faces')
+        # Parameters combinations to check
+        param_grid = {
+        	'method':['ahash', 'pca', 'hog', None],
+        	'embedding':['tsne', None],
+        	'cluster_space' : ['high', 'low'],
+        	'grayscale' : [True, False],
+            'dim' : [(8,8), (128,128), (256,256)],
+            'data' : [Xflowers, Xdigits]
+        	}
+        # Make the combinatinos
+        allNames = param_grid.keys()
+        combinations = list(it.product(*(param_grid[Name] for Name in allNames)))
+        # Iterate over all combinations
+        for i, combination in enumerate(tqdm(combinations)):
+            # init
+            cl = Clustimage(method=combination[0], embedding=combination[1], grayscale=combination[3], dim=combination[4], verbose=30, params_pca={'n_components':50})
+            # Preprocessing and feature extraction
+            assert cl.fit_transform(combination[5], cluster_space=combination[2])
+
