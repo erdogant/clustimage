7431211cb6a01719ac7a3345e143ffbee2914299
erdogant <erdogant@gmail.com>
2021-12-28 11:54:40 +0100

    new version

--- clustimage/__init__.py
+++ clustimage/__init__.py
@@ -10,7 +10,7 @@ from clustimage.clustimage import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.3.11'
+__version__ = '1.3.12'
 
 # module level doc-string
 __doc__ = """
