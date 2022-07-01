e0dd1483b1b48c63af0d9eae8467fd2a5338961f
erdogant <erdogant@gmail.com>
2022-07-01 16:33:50 +0200

    1.5.5

--- clustimage/__init__.py
+++ clustimage/__init__.py
@@ -9,7 +9,7 @@ from clustimage.clustimage import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.5.4'
+__version__ = '1.5.5'
 
 # module level doc-string
 __doc__ = """
